467c6a169d88ffc869aa4495956f746393f9196d
Vaibhav dwivedi <vickydwivedi989@gmail.com>
2023-04-24 18:04:45 +0530

    url upload instead of image

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask, request
 import pytesseract
+from pytesseract import TesseractError
+import urllib.request
 
 app = Flask(__name__)
 
-@app.route('/upload-image', methods = ['POST'])
+@app.route('/upload_image', methods = ['POST'])
 def image_to_text():
-    file = request.files['image']
-    file.save('uploaded-image')
-    text = pytesseract.image_to_string('uploaded-image',lang='eng')
-    return (text)
+
+    image_url = request.form.get('image_url')
+
+    print(image_url)
+    urllib.request.urlretrieve(image_url, 'uploaded_image')
+
+    # Save the image data to a file and perform OCR
+    try:
+        text = pytesseract.image_to_string('uploaded_image', lang='eng')
+    except TesseractError as error:
+        return str(error), 500
+
+    return text
 
 
