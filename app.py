5152e02ddddf35d59e7494e59f6f11043a5e2d3e
Vaibhav dwivedi <vickydwivedi989@gmail.com>
2023-04-24 18:13:37 +0530

    code cleaning

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 import pytesseract
-from pytesseract import TesseractError
 import urllib.request
 
 app = Flask(__name__)
@@ -10,14 +9,9 @@ def image_to_text():
 
     image_url = request.form.get('image_url')
 
-    print(image_url)
     urllib.request.urlretrieve(image_url, 'uploaded_image')
 
-    # Save the image data to a file and perform OCR
-    try:
-        text = pytesseract.image_to_string('uploaded_image', lang='eng')
-    except TesseractError as error:
-        return str(error), 500
+    text = pytesseract.image_to_string('uploaded_image', lang='eng')
 
     return text
 
