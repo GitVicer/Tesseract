6d84257d62c2246f3da62a9d8dad74105fee50fd
Vaibhav dwivedi <vickydwivedi989@gmail.com>
2023-04-25 15:15:49 +0530

    phone number extraction

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ import urllib.request
 
 app = Flask(__name__)
 
+def isNumber(str):
+    
+    for char in str:
+        print(char.isdigit())
+        if char.isdigit() != True and char != '-' and char != '+' and char != " ":
+            return False
+    return True 
+
 @app.route('/upload_image', methods = ['POST'])
 def image_to_text():
 
@@ -12,7 +20,21 @@ def image_to_text():
     urllib.request.urlretrieve(image_url, 'uploaded_image')
 
     text = pytesseract.image_to_string('uploaded_image', lang='eng')
+    
+    index = text.index('+')
+    
+    num =  text[index:index+15]
+    
+    if isNumber(num):
+        return num
+    else:
+        return "This program searches for a valid number on the fact that if there is a '+' followed by digits and '-'"
+            
 
-    return text
+
+
+
+
+    
 
 
